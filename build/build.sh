1e5c898f9914c69dbc6c7da468f633414bbd3531
Manuel Lopez <jmlopez.rod@gmail.com>
2021-06-17 20:49:39 -0500

    (refactor) move files to accomodate bash scripts

--- build/build.sh
+++ build/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -xeuo pipefail
 
-export PYTHONPATH="${PWD}/src"
+export PYTHONPATH="${PWD}/packages/python"
 
 # static checks
-mypy ./src/m/__main__.py
+mypy ./packages/python/m/__main__.py
 
 # tests
-./src/tests/run.sh
+./packages/python/tests/run.sh
 
 # pylint
 pylint m --rcfile=.pylintrc
 
 # pep8
-pycodestyle --format=pylint src
+pycodestyle --format=pylint packages/python
