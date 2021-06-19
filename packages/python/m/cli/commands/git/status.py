f0a3fa6fd2f961053837608827b20f9286e8863d
Manuel Lopez <jmlopez.rod@gmail.com>
2021-06-19 08:54:59 -0500

    Provide a git status command
    It displays a short status keyword. Note, it will not display if there are stashed changes.

---
+++ packages/python/m/cli/commands/git/status.py
@@ -0,0 +1,36 @@
+import inspect
+from ...utils import call_main
+
+
+def add_parser(sub_parser, raw):
+    desc = """
+        Display a single word representing the current git status.
+
+            $ m git status
+            clean
+
+        Statuses:
+
+            unknown
+            untracked
+            stash
+            clean
+            ahead
+            behind
+            staged
+            dirty
+            diverged
+            ?
+    """
+    sub_parser.add_parser(
+        'status',
+        help='display the current git status',
+        formatter_class=raw,
+        description=inspect.cleandoc(desc)
+    )
+
+
+def run(_):
+    # pylint: disable=import-outside-toplevel
+    from m import git
+    return call_main(git.get_status, [], print_raw=True)
--- packages/python/m/git.py
+++ packages/python/m/git.py
@@ -1,4 +1,5 @@
 from .core import subprocess
+from .core.fp import one_of
 
 
 def get_branch():
@@ -14,3 +15,30 @@ def get_first_commit_sha():
 def get_current_commit_sha():
     """Returns a `OneOf` object containing the current commit."""
     return subprocess.eval_cmd('git rev-parse HEAD')
+
+
+def _extract_status(msg: str) -> str:
+    matches = [
+        ('Untracked files', 'untracked'),
+        ('Your branch is ahead', 'ahead'),
+        ('Your branch is behind', 'behind'),
+        ('working tree clean', 'clean'),
+        ('Changes to be committed', 'staged'),
+        ('Changed but not updated', 'dirty'),
+        ('Changes not staged', 'dirty'),
+        ('Unmerged paths', 'dirty'),
+        ('diverged', 'diverged'),
+    ]
+    for substr, key in matches:
+        if substr in msg:
+            return key
+    return '?'
+
+
+def get_status():
+    """Return a `OneOf` containing the current git status."""
+    res = subprocess.eval_cmd('git status')
+    return one_of(lambda: [
+        _extract_status(msg)
+        for msg in res
+    ])
