a73a8cd73a97586079a611db368f375bea11241a
Manuel Lopez <jmlopez.rod@gmail.com>
2021-06-29 08:47:21 -0500

    (chore) publishing pr versions
    Eventually we'll need to make a cli command to safely delete packages published for prs that have been closed or merged.

---
+++ build/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -xeuo pipefail
+
+# source the m environment
+m ci env m
+source m/.m/env.list
+export $(cut -d= -f1 m/.m/env.list)
+
+
+rm -rf .stage
+
+cp -r ./packages ./.stage
+cp ./package.json ./.stage/package.json
+
+find .stage | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+sed -i -e "s/0.0.0-PLACEHOLDER/$M_TAG/g" ./.stage/package.json
+
+cd .stage && npm pack
+
+# Only publish with the CI tool
+[ "$M_CI" == "True" ] || exit 0
+npm publish *.tgz
