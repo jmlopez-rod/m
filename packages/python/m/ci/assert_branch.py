55f2f68346a6a1dcc9968c40cd60bd0656b200b6
Manuel Lopez <jmlopez.rod@gmail.com>
2021-08-31 20:12:23 -0500

    assert_branch should have empty output

--- packages/python/m/ci/assert_branch.py
+++ packages/python/m/ci/assert_branch.py
@@ -20,6 +20,8 @@ def _verify_branch(
         req_branch = config.m_flow.master_branch
     elif workflow == Workflow.GIT_FLOW:
         flow = config.git_flow
+        # releases are made from develop branch
+        # hotfixes are made from master branch
         req_branch = (
             flow.develop_branch
             if assertion_type == 'release'
@@ -43,7 +45,7 @@ def _verify_branch(
 def assert_branch(assertion_type: str, m_dir: str) -> OneOf[Issue, str]:
     """Make sure git is using the correct branch based on the workflow."""
     return one_of(lambda: [
-        str(config)
+        ''
         for config in read_config(m_dir)
         for branch in git.get_branch()
         for _ in _verify_branch(config, branch, assertion_type)
