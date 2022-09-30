c708615f73f4ad0655d41f730a30cb3da9d7021a
Manuel Lopez <jmlopez.rod@gmail.com>
2022-09-29 23:22:36 -0500

    I think there is a mistake on that rule
    I'll check on it at a later time, for now things should pass I hope

--- packages/python/m/ci/git_env.py
+++ packages/python/m/ci/git_env.py
@@ -172,7 +172,7 @@ class GitEnv(JsonStr):
 def _build_tag_prefix(config: Config) -> str:
     if config.build_tag_with_version:
         return f'{config.version}-'
-    # TODO: Allow to set a specific build tag prefix
+    # Need to allow to set a specific build tag prefix
     return '999.0.0-'
 
 
