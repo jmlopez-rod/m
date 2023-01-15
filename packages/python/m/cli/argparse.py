c7d6a51ff2f77b36fb4137a1eddf2303f12fa95e
Manuel Lopez <jmlopez.rod@gmail.com>
2023-01-15 01:56:02 -0600

    use pydantic to simplify argparse

--- packages/python/m/cli/argparse.py
+++ packages/python/m/cli/argparse.py
@@ -1,14 +1,22 @@
-from argparse import ArgumentParser
+import argparse
 from inspect import cleandoc
 from types import MappingProxyType
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, TypeVar
+
+import pydantic
 
 STORE_TRUE = MappingProxyType({'action': 'store_true'})
 
 Arg = Tuple[List[str], str, Any]
 
+BaseModelT = TypeVar('BaseModelT', bound=pydantic.BaseModel)
+MISSING = TypeVar('MISSING')
 
-def add_arguments(parser: ArgumentParser, args: Tuple[Arg, ...]) -> None:
+
+def add_arguments(
+    parser: argparse.ArgumentParser,
+    args: Tuple[Arg, ...],
+) -> None:
     """Add arguments to the parser.
 
     Args:
@@ -22,3 +30,116 @@ def add_arguments(parser: ArgumentParser, args: Tuple[Arg, ...]) -> None:
     """
     for names, help_str, extra in args:
         parser.add_argument(*names, help=cleandoc(help_str), **extra)
+
+
+def argument_name(name: str) -> str:
+    """Normalize an argument name.
+
+    Args:
+        name: Name of the argument.
+
+    Returns:
+        Normalized name of the argument.
+    """
+    return f"--{name.replace('_', '-')}"
+
+
+def argument_description(
+    description: str,
+    default: Any | None = MISSING,
+) -> str:
+    """Append default value to argument description.
+
+    Args:
+        description: argument description.
+        default: Argument's default value.
+
+    Returns:
+        The description of the argument.
+    """
+    default = f'(default: {default})' if default is not MISSING else ''
+    return f'{description} {default}'.strip()
+
+
+def add_model(
+    parser: argparse.ArgumentParser,
+    model: type[pydantic.BaseModel],
+) -> None:
+    """Add a pydantic model to an argparse ArgumentParser.
+
+    Args:
+        parser: The ArgumentParser instance.
+        model: The pydantic model declaring the cli options.
+    """
+    schema = model.schema()
+    parser.description = schema['description']
+    parser.formatter_class = argparse.RawTextHelpFormatter
+
+    for name, field in schema['properties'].items():
+        field_default = field.get('default', MISSING)
+        is_positional = field.get('positional', False)
+        field_type = field.get('type', None)
+        args: dict[str, Any] = {
+            'help': argument_description(
+                field['description'],
+                MISSING if field_type == 'boolean' else field_default,
+            ),
+        }
+        _add_validator(args, field)
+
+        if is_positional:
+            _handle_positional(args, field)
+        else:
+            field_name = argument_name(name)
+            args['required'] = field.get('required', False)
+            args['type'] = str
+
+        field_name = name
+        if field_type == 'boolean':
+            field_name = _handle_boolean(args, field, name)
+        parser.add_argument(field_name, **args)
+
+
+def _add_validator(args: dict, field: dict) -> None:
+    if field.get('validator', None):
+        args['type'] = field['validator']
+
+
+def _handle_positional(args: dict, field: dict) -> None:
+    is_required = field.get('required', False)
+    if not is_required:
+        args['nargs'] = '?'
+
+
+def _handle_boolean(args: dict, field: dict, name: str) -> str:
+    args.pop('type')
+    if field.get('default', False):
+        args['action'] = 'store_false'
+        args['dest'] = name
+        return argument_name(f'no-{name}')
+    args['action'] = 'store_true'
+    return argument_name(name)
+
+
+def namespace_to_dict(namespace: argparse.Namespace) -> dict[str, Any]:
+    """Convert a namespace to a dictionary.
+
+    Args:
+        namespace: Namespace instance to convert.
+
+    Returns:
+        A dictionary generated from namespace.
+    """
+    dictionary = vars(namespace)
+    for (key, value) in dictionary.items():
+        if isinstance(value, argparse.Namespace):
+            dictionary[key] = namespace_to_dict(value)
+    return dictionary
+
+
+def cli_options(
+    model: type[BaseModelT],
+    namespace: argparse.Namespace,
+) -> BaseModelT:
+    arg_dict = namespace_to_dict(namespace)
+    return model.parse_obj(arg_dict)
--- packages/python/m/cli/commands/json.py
+++ packages/python/m/cli/commands/json.py
@@ -1,48 +1,58 @@
-from inspect import cleandoc as cdoc
-from typing import Tuple
+from typing import Any
 
-from ..argparse import STORE_TRUE, Arg, add_arguments
+from pydantic import BaseModel, Field
+
+from ..argparse import add_model, cli_options
 from ..validators import validate_json_payload
 
-DESC = """
-    Format a json payload.
+
+class Arguments(BaseModel):
+    """Format a json payload.
 
         $ echo '{"a":99}' | m json
         {
-            "a": 99
+          "a": 99
         }
 
     similar to `python -m json.tool` but instead it uses 2 spaces
     for indentation.
-"""
-ARGS: Tuple[Arg, ...] = (
-    (
-        ['payload'],
-        'json data: @- (stdin), @filename (file), string. Defaults to @-',
-        {'type': validate_json_payload, 'nargs': '?', 'default': '@-'},
-    ),
-    (
-        ['--sort-keys'],
-        'sort the output of dictionaries alphabetically by key',
-        {**STORE_TRUE},
-    ),
-)
 
+    It is worth noting that if you have access to `jq` or `yq` then
+    it should be used instead of `m json`.
 
-def add_parser(sub_parser, raw):
-    parser = sub_parser.add_parser(
-        'json',
-        help='format json data',
-        formatter_class=raw,
-        description=cdoc(DESC),
+        $ echo '{"a":99}' | jq
+        {
+          "a": 99
+        }
+
+    - jq: https://stedolan.github.io/jq/manual/
+    - yq: https://mikefarah.gitbook.io/yq/
+    """  # noqa
+
+    payload: Any | None = Field(
+        '@-',
+        description='json data: @- (stdin), @filename (file), string',
+        validator=validate_json_payload,
+        positional=True,
     )
-    add_arguments(parser, ARGS)
+
+    sort_keys: bool = Field(
+        False,
+        description='sort the output of dictionaries alphabetically by key',
+    )
+
+
+def add_parser(sub_parser, _raw):
+    parser = sub_parser.add_parser('json', help='format json data')
+    add_model(parser, Arguments)
 
 
 def run(arg):
     # pylint: disable=import-outside-toplevel
     import json
     import sys
-    json.dump(arg.payload, sys.stdout, indent=2, sort_keys=arg.sort_keys)
+
+    opt = cli_options(Arguments, arg)
+    json.dump(opt.payload, sys.stdout, indent=2, sort_keys=opt.sort_keys)
     sys.stdout.write('\n')
     return 0
