521d6d48d15373d2dc7254ed5e7a1612928bb4e2
Manuel Lopez <jmlopez.rod@gmail.com>
2023-01-20 17:40:40 +0000

    remove add_parser in assert_branch cli

--- packages/python/m/cli/commands/ci/assert_branch.py
+++ packages/python/m/cli/commands/ci/assert_branch.py
@@ -1,31 +1,35 @@
-import inspect
+from m.cli import add_arg, command, run_main
+from pydantic import BaseModel, Field
 
-from ...utils import run_main
 
+class Arguments(BaseModel):
+    """Fail command when working on a non valid branch for a release/hotfix.
 
-def add_parser(sub_parser, raw):
-    desc = """
-        Used during a release setup or hotfix setup. We want to make sure
-        that we are working on the correct branch depending on verification
-        we want to make and the workflow that we are using.
+    Used during a release setup or hotfix setup. We want to make sure
+    that we are working on the correct branch depending on release type
+    we want to make and the workflow that we are using.
     """
-    parser = sub_parser.add_parser(
-        'assert_branch',
-        help='assert that we are working on the correct branch',
-        formatter_class=raw,
-        description=inspect.cleandoc(desc),
+
+    type: str = Field(
+        proxy=add_arg(
+            '--type',
+            required=True,
+            choices=['release', 'hotfix'],
+            help='verification type',
+        ),
     )
-    add = parser.add_argument
-    add(
-        '--type',
+    m_dir: str = Field(
+        description='m project directory',
+        positional=True,
         required=True,
-        choices=['release', 'hotfix'],
-        help='verification type',
     )
-    add('m_dir', type=str, help='m project directory')
 
 
-def run(arg):
-    # pylint: disable=import-outside-toplevel
-    from ....ci.assert_branch import assert_branch
+@command(
+    name='assert_branch',
+    help='assert that we are working on the correct branch',
+    model=Arguments,
+)
+def run(arg: Arguments):
+    from m.ci.assert_branch import assert_branch
     return run_main(lambda: assert_branch(arg.type, arg.m_dir))
