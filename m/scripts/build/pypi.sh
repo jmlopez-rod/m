f784a3b1a1901b1c4669be9e0847860767d9b14e
Manuel Lopez <jmlopez.rod@gmail.com>
2023-02-01 04:38:58 +0000

    using M_PY_TAG

--- m/scripts/build/pypi.sh
+++ m/scripts/build/pypi.sh
@@ -20,11 +20,10 @@ cp LICENSE README.md "./$target"
 
 find "./$target" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 rm -rf "./$target/src/tests"
-sed -i -e "s/0.0.0-PLACEHOLDER/$M_TAG/g" "./$target/setup.py"
-sed -i -e "s/0.0.0-PLACEHOLDER/$M_TAG/g" "./$target/src/m/version.py"
+sed -i -e "s/0.0.0-PLACEHOLDER/$M_PY_TAG/g" "./$target/setup.py"
+sed -i -e "s/0.0.0-PLACEHOLDER/$M_PY_TAG/g" "./$target/src/m/version.py"
 
-# Skipping python build for now - need to provie $M_PY_TAG
-# (
-  # cd "$target"
-  # python3 -m build
-# )
+(
+  cd "$target"
+  python3 -m build
+)
