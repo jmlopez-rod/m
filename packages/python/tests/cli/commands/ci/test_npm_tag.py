4bf763bb142cc31ec06f110b7e8d9755bef78220
Manuel Lopez <jmlopez.rod@gmail.com>
2023-02-09 12:24:27 -0600

    (hotfix) 0.12.2 (#49)
    - Fix npm_tag cli

--- packages/python/tests/cli/commands/ci/test_npm_tag.py
+++ packages/python/tests/cli/commands/ci/test_npm_tag.py
@@ -7,9 +7,17 @@ from tests.cli.conftest import TCase, assert_streams, run_cli
     TCase(cmd=f'm ci npm_tag {m_tag}', expected=expected)
     for m_tag, expected in [
         ('0.0.1', 'latest'),
+        ('1.22.333', 'latest'),
         ('0.0.1-pr123.b123', 'pr123'),
         ('0.0.1-rc123.b123', 'next'),
+        ('10.0.0-rc123.b123', 'next'),
+        ('0.100.0-rc123.b123', 'next'),
+        ('0.0.1000-rc123.b123', 'next'),
         ('0.0.1-hotfix123.b123', 'next'),
+        ('10.0.0-hotfix123.b123', 'next'),
+        ('0.100.0-hotfix123.b123', 'next'),
+        ('0.0.1000-hotfix123.b123', 'next'),
+        ('99.999.9999-hotfix123.b123', 'next'),
         ('0.0.1-master.b123', 'master'),
         ('0.0.1-develop.b123', 'develop'),
         ('0.0.1-branch.b123', 'branch'),
