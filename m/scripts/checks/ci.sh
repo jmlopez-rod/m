833acc926afe5051fb291dfb8a465d4836211ce5
Manuel Lopez <jmlopez.rod@gmail.com>
2023-02-19 05:06:31 +0000

    add blocks

--- m/scripts/checks/ci.sh
+++ m/scripts/checks/ci.sh
@@ -8,15 +8,19 @@ export PATH="${PWD}/packages/bash/lib:$PATH"
 m ci celt -t flake8 -c @allowed_errors.json < <(flake8 packages/python/m)
 
 # static checks
+m message open 'mypy' 'static checks'
 mypy ./packages/python/m
 mypy ./packages/python/tests
 
 # tests
+m message sibling_block 'mypy' 'tests' 'run all tests'
 ./packages/python/tests/run.sh
 
 # pylint
+m message sibling_block 'tests' 'pylint' 'run pylint on src code'
 m ci celt -t pylint -m 10 -c @allowed_errors.json < <(pylint ./packages/python/m --rcfile=.pylintrc -f json)
 
 # lint tests
+m message sibling_block 'pylint' 'lint-tests' 'run linters on tests'
 m ci celt -t flake8 -c @allowed_errors_tests.json < <(flake8 packages/python/tests --config .flake8-tests)
 m ci celt -t pylint -m 10 -c @allowed_errors_tests.json < <(pylint ./packages/python/tests --rcfile=packages/python/tests/.pylintrc -f json)
