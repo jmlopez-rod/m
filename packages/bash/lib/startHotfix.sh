d5a4799646e696ae7cfa35cf231e2c6ff02cdc42
Manuel Lopez <jmlopez.rod@gmail.com>
2023-02-24 20:33:38 +0000

    deprecate startRelease and startHotfix bash scripts

--- packages/bash/lib/startHotfix.sh
+++ packages/bash/lib/startHotfix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# @depracated - remove when moving to m 1.0.0
+
 # Adding to path so that npx can work: https://superuser.com/a/39995
 pathadd() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
@@ -8,27 +10,5 @@ pathadd() {
 }
 pathadd "${PWD}/node_modules/@PACKAGE_SCOPE/m/bash/lib"
 
-set -euxo pipefail
-
-# Making sure we are releasing from the proper branch
-#  git_flow: master
-#  m_flow: master
-m ci assert_branch --type hotfix m
-
-# Make sure we are in a clean state
-[ "$(m git status)" == "clean" ] \
-  || m message error 'hotfixSetup.sh can only run in a clean git state'
-
-
-# Gather info
-owner=$(m jsonq @m/m.json owner)
-repo=$(m jsonq @m/m.json repo)
-currentVersion=$(m github latest_release --owner "$owner" --repo "$repo")
-newVersion=$(m ci bump_version --type hotfix "$currentVersion")
-
-# Swith to release branch
-git checkout -b "hotfix/$newVersion" \
-  || m message error "Unable to switch to hotfix/$newVersion branch"
-
-# Update CHANGELOG and m.json files
-m ci release_setup './m' "$newVersion"
+m message warn 'startHotfix is deprecated - use `m start_hotfix`'
+m start_hotfix
--- packages/bash/lib/startRelease.sh
+++ packages/bash/lib/startRelease.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# @depracated - remove when moving to m 1.0.0
+
 # Adding to path so that npx can work: https://superuser.com/a/39995
 pathadd() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
@@ -8,27 +10,5 @@ pathadd() {
 }
 pathadd "${PWD}/node_modules/@PACKAGE_SCOPE/m/bash/lib"
 
-set -euxo pipefail
-
-# Making sure we are releasing from the proper branch
-#  git_flow: develop
-#  m_flow: master
-m ci assert_branch --type release m
-
-# Make sure we are in a clean state
-[ "$(m git status)" == "clean" ] \
-  || m message error 'releaseSetup.sh can only run in a clean git state'
-
-
-# Gather info
-owner=$(m jsonq @m/m.json owner)
-repo=$(m jsonq @m/m.json repo)
-currentVersion=$(m github latest_release --owner "$owner" --repo "$repo")
-newVersion=$(m ci bump_version --type release "$currentVersion")
-
-# Swith to release branch
-git checkout -b "release/$newVersion" \
-  || m message error "Unable to switch to release/$newVersion branch"
-
-# Update CHANGELOG and m.json files
-m ci release_setup './m' "$newVersion"
+m message warn 'startRelease is deprecated - use `m start_release`'
+m start_release
