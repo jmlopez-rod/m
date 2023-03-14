7ea1f2439f66d238a9d7c0e2e1c8a7fc45c05c39
Manuel Lopez <jmlopez.rod@gmail.com>
2023-03-14 19:28:21 +0000

    (release) 0.17.0

--- packages/python/m/core/yaml.py
+++ packages/python/m/core/yaml.py
@@ -16,9 +16,11 @@ def _tuple_presenter(dumper: yaml.SafeDumper, py_data: tuple):
 
 def _str_presenter(dumper: yaml.SafeDumper, py_data: str):
     if '\n' in py_data:  # check for multiline string
+        # tab characters do not play along with yaml
+        # found this when trying to print the output of `git status`.
         return dumper.represent_scalar(
             f'{tag_prefix}:str',
-            py_data.replace('\r\n', '\n'),
+            py_data.replace('\r\n', '\n').replace('\t', '    '),
             style='|',
         )
     return dumper.represent_scalar(f'{tag_prefix}:str', py_data)
