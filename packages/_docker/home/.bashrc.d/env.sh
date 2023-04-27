e73fe529fc8c4368211f345cb6e0e80d340e12fc
Manuel Lopez <jmlopez.rod@gmail.com>
2023-04-26 22:47:30 -0500

    split ci workflow into several jobs
    testing out running tests separately

--- packages/_docker/home/.bashrc.d/env.sh
+++ packages/_docker/home/.bashrc.d/env.sh
@@ -9,5 +9,4 @@ export VIRTUAL_ENV="/opt/venv/m"
 export PATH="$VIRTUAL_ENV/bin:$PATH"
 . "$VIRTUAL_ENV/bin/activate"
 
-export PATH="${CONTAINER_WORKSPACE}/packages/bash/lib:$PATH"
 export PYTHONPATH="${CONTAINER_WORKSPACE}/packages/python"
