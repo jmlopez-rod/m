b5d8e3fb28606be093026c245cafe2c8d95528bc
Manuel Lopez <jmlopez.rod@gmail.com>
2023-04-30 20:10:12 +0000

    trying it out

--- actions/env/ensure-m.sh
+++ actions/env/ensure-m.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+mVersion="${1:-latest}"
+
 set -exuo pipefail
 
 mPath=$(which m || echo '')
 if [ "$mPath" == '' ]; then
-  pip install jmlopez-m
-else
-  echo 'installed'
+  if [ "$mVersion" == 'latest' ]; then
+    pip install jmlopez-m
+  else
+    pip install jmlopez-m==$mVersion
+  fi
 fi
