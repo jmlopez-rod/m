e74e4a514729cdb5d6a72364a95b243c660d7b0e
Manuel Lopez <jmlopez.rod@gmail.com>
2024-03-26 18:50:40 +0000

    add tests for new manifest creation

---
+++ packages/python/tests/blueprints/multi-arch-buildx/fixtures/ci/m-image1.manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+docker buildx imagetools create \
+  -t ghcr.io/repo-owner/m-image1:1.1.1 \
+  -t ghcr.io/repo-owner/m-image1:latest \
+  -t ghcr.io/repo-owner/m-image1:v1 \
+  -t ghcr.io/repo-owner/m-image1:v1.1 \
+  ghcr.io/repo-owner/amd64-m-image1:1.1.1 \
+  ghcr.io/repo-owner/arm64-m-image1:1.1.1
